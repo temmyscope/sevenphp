5b1abb9323fc7174eee2732ef264d4d68ae4a5b1
temmyscope <esdentp@gmail.com>
2020-07-23 22:44:05 -0700

    Addition of migration Engine

--- app/Apps.php
+++ app/Apps.php
@@ -1,7 +1,7 @@
 <?php
 namespace App;
 
-use App\Providers\{Model, Schema};
+use App\Providers\{Model};
 
 class Apps extends Model
 {
@@ -11,21 +11,5 @@ class Apps extends Model
 	protected static $fulltext = [];
 
 	protected static $fetchable = [ 'id', 'name', 'account_balance', 'is_verified', 'created_at' ];
-
-	public function migrate(Schema $schema)
-	{
-		return [
-			'id' => $schema->integer($max_length=10)->is_null(true)->primary() || $schema->integer($max_length=10, $null = false, $key='primary'),
-			'name' => $schema->string($max_length=1125)->is_null(false) || $schema->string($max_length=125, $null=true),
-			'account_balance' => $schema->float($max_length=10)->is_null(false) || $schema->string($max_length=125, $null=true),
-			'is_verified' => $schema->boolean()->default() || $schema->boolean($default=) || $schema->oneOf(['true', 'false'])->default('false'),
-			'created_at' => $schema->time()->auto_generate(true) || $schema->time($auto_generate=true)
-		];
-	}
-
-	public function populate()
-	{
-		return [ [ 'name' => , 'account_balance' => , 'is_verified' => ],  ];
-	}
 
 }
--- app/Providers/Schema.php
+++ app/Providers/Schema.php
@@ -1,104 +1,140 @@
 <?php
 namespace App\Providers;
 
+use {MigrationEngine, Model};
+
+use \PDO;
+use \PDOException;
+
 class Schema{
 
-	public function __migrations()
+	public function __construct()
 	{
-		$migrations = app()->get('ENGINE')['MIGRATIONS'];
-		$arr = [];
-		foreach ($migrations as $key) {
-			$arr[] = (new $key)->migrate($this);
+		$this->mige = new MigrationEngine();
+		$this->engine = app()->get('ENGINE');
+		$this->migrable = $this->engine['MIGRATIONS'];
+	}
+
+	public static function init()
+	{
+		return new self();
+	}
+
+	public function run()
+	{	
+		try {
+			$db = str_replace("pdo_", '', Model::$driver); $server = Model::$host; $dbname = Model::$dbname;
+	    	$conn = new PDO("$db:host=$server;dbname=$dbname", Model::$user, Model::$password);
+	    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    	$drop = $this->mige->drop();
+	    	if( !empty($drop) ){
+	    		$sql = "";
+		    	foreach ($drop as $key => $value) {
+		    		$sql .= "DROP TABLE {$value}; ";
+		    	}
+		    	$conn->exec($sql);
+		    	echo "Tables have been dropped";
+	    	}
+
+	    	$conn->exec($this->parser());
+	    	echo "Migration created successfully";
+
+	    	$this->InsertionIfExists();
+
+	    }catch(PDOException $e){
+		    echo $sql . "<br>" . $e->getMessage();
+	    }
+	}
+
+	public function InsertionIfExists()
+	{
+		$data = $this->mige->populate();
+		if (!empty($data)) {
+			foreach ($data as $table => $entry) {
+				Model::setTable($table)->insert($entry);
+			}
 		}
+		echo "Insertions Successful.";
 	}
 
-	public function __migration($model)
+	public function parser()
 	{
-		$mig = (new 'App\\'.$model)->migrate($this);
+		$mig = $this->mige->migrate($this);
+		$chs = $this->engine['CHARSET']; $col = $this->engine['COLLATE'];
+		$queue = ["ALTER TABLE :table ADD PRIMARY KEY (id),"];
+		$_sql = "";
+		if( !empty($mig) ){
+			foreach ($mig as $table => $columns) {
+				if (in_array($table, $this->migrable)) {
+					$_sql .= "CREATE TABLE `{$table}` (
+					id int(10) NOT NULL AUTO_INCREMENT,
+					";
+					foreach ($columns as $key => $value) {
+						$_sql .= str_replace(":column", $key, $value[0]);
+						if (!empty($value[1])) {
+							$queue[] = str_replace(":column", $key, str_replace(":table", $table, $value[1]) );
+						}
+					}
+					foreach ($queue as $key => $value) {
+						$_sql .= str_replace(':table', $table, $value);
+					}
+					$_sql .= (!empty($chs) && !empty($col) ) ? ") CHARSET={$chs} COLLATE $col; 
+					": "); 
+					";
+				}
+			}
+		}
+		return $_sql;
 	}
 
-	public function type($value='', int $max_length)
+	public function integer($max_length=10)
 	{
-		# code...
+		$t = "";
+		return [":column int({$max_length}) NOT NULL,", $t];
+	}
+	public function double($max_length=10)
+	{
+		return [ ":column double,", ""];
+	}
+	public function float($max_length=10)
+	{
+		return [ ":column float({$max_length}),", ""];
+	}
+	public function string($max_length=10, $null = false, $key='primary')
+	{
+		$null = ($null === false) ? "NOT NULL" : "NULL";
+		$t = "";
+		if(!empty($key)){
+			$key = strtolower($key);
+			if ( $key === 'unique' ){
+				$t = "ALTER TABLE :table ADD UNIQUE :column (:column),";
+			}elseif ( $key === 'fulltext' ){
+				$t = "ALTER TABLE :table ADD FULLTEXT KEY :column (:column),";
+			} elseif ( $key === 'index' ){
+				$t = "CREATE INDEX :column ON :table (:column),";
+			}
+		}
+		if ($max_length > 63000) {
+			$type =  "text";
+		}elseif ($max_length <= 63000) {
+			$type = "var_char({$max_length})";
+		}elseif ($max_length < 18) {
+			$type =  "char({$max_length})" ;
+		}
+		return [ ":column {$type} {$null}", $t ];
+	}
+	public function oneOf(array $options, $default)
+	{
+		$options  = implode(', ', $options);
+		return [ ":column enum({$options}) NOT NULL DEFAULT '{$default}',", ""];
+	}
+	public function datetime()
+	{
+		return [ ":column enum({$options}) NOT NULL DEFAULT CURRENT_TIMESTAMP,", ""];
+	}
+	public function foreign_key($table, $column)
+	{
+		return ["", "ALTER TABLE :table ADD FOREIGN KEY (:column) REFERENCES {$table}({$column}),"];
 	}
 
-	public function integer( int $max_length)
-	{
-		# code...
-	}
-
-	public function float( int $max_length)
-	{
-		# code...
-	}
-
-	public function string( int $max_length)
-	{
-		# code...
-	}
-
-	public function oneOf(array $options)
-	{
-		
-	}
-
-	public function boolean()
-	{
-		# code...
-	}
-
-	public function date()
-	{
-		# code...
-	}
-
-	public function time()
-	{
-		
-	}
-
-	public function year()
-	{
-		
-	}
-
-	public function timestamp()
-	{
-		
-	}
-
-	public function comment($value='')
-	{
-		# code...
-	}
-
-	public function is_null()
-	{
-		# code...
-	}
-
-	public function primary()
-	{
-		# code...
-	}
-
-	public function default($value)
-	{
-		# code...
-	}
-
-	public function auto_generate(true)
-	{
-		# code...
-	}
-
-	public function key('')
-	{
-		# code...
-	}
-
-	public function index('')
-	{
-		# code...
-	}
 }
